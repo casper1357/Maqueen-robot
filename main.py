bcfa1112be9d9312ead99f52bcf7b78a7f3d86de
FrederikSperling <61236493+FrederikSperling@users.noreply.github.com>
2021-11-08 08:46:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 # CCW backwards
 # CW forwards
+strip: neopixel.Strip = None
+
+def on_button_pressed_a():
+    music.play_melody("C - C F C B A G ", 120)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_forever():
-    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    global strip
+    strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+    strip.show_color(neopixel.colors(NeoPixelColors.INDIGO))
+    basic.show_leds("""
+                . # # # .
+                # # . . .
+                # . # . .
+                # . . # .
+                . . . . #
+    """)
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+    if input.compass_heading() == 90:
+        maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 60)
 basic.forever(on_forever)
